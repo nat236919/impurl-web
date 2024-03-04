0bec291d7b2df706e3556fb50a99403090480292
nuttaphat <nuttaphat.aruno@gmail.com>
2024-03-04 20:44:30 +0800

    chore: install and config tailwindcss (#1)

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,17 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+
+import { resolve } from "path";
+
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  alias: {
+    "@": resolve(__dirname, "/"),
+  },
+  css: ['~/assets/scss/main.scss'],
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  }
 })
