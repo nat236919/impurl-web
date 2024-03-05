8ead7bb8931b9822102ea39a10f9a497327e3e37
nuttaphat <nuttaphat.aruno@gmail.com>
2024-03-05 22:32:28 +0800

    feat: add runtimeConfig vars from .env (#1)

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -13,5 +13,13 @@ export default defineNuxtConfig({
       tailwindcss: {},
       autoprefixer: {},
     },
+  },
+  runtimeConfig: {
+    // The private keys which are only available server-side
+    apiSecretKey: process.env.API_SECRET_KEY,
+    // Keys within public are also exposed client-side
+    public: {
+      apiHost: process.env.API_HOST,
+    }
   }
 })
--- pages/index.vue
+++ pages/index.vue
@@ -3,3 +3,15 @@
         <h1>Home Page</h1>
     </div>
 </template>
+
+<script setup>
+console.log("Loading runtimeConfig in index.vue")
+const runtimeConfig = useRuntimeConfig()
+console.log(runtimeConfig)
+</script>
+
+<script>
+</script>
+
+<style>
+</style>
