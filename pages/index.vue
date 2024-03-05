49d11fa8fcaf7fe572d5feed09ebd49e26a379e6
nuttaphat <nuttaphat.aruno@gmail.com>
2024-03-05 23:08:33 +0800

    feat: add CTA to index page (#1)

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,25 @@
 <template>
-    <div>
-        <h1>Home Page</h1>
+    <div class="relative max-w-5xl mx-auto pt-20 sm:pt-24 lg:pt-32">
+        <h1
+            class="text-slate-900 font-extrabold text-4xl sm:text-5xl lg:text-6xl tracking-tight text-center dark:text-white">
+            impURL
+        </h1>
+        <h3 class="mt-4 text-slate-600 text-center text-lg sm:text-xl max-w-3xl mx-auto dark:text-slate-400">
+            Where magic makes links shorter and sweeter
+        </h3>
+        <div class="mt-6 sm:mt-10 flex justify-center space-x-6 text-sm">
+            <div>
+                <div class="mt-2">
+                    <input id="url" name="url" type="url" v-model="url" required
+                        class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
+                </div>
+            </div>
+            <button
+                class="bg-slate-900 hover:bg-slate-700 focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50 text-white font-semibold h-12 px-6 rounded-lg w-full flex items-center justify-center sm:w-auto dark:bg-sky-500 dark:highlight-white/20 dark:hover:bg-sky-400"
+                @click="shortenURL()">
+                Shrink!
+            </button>
+        </div>
     </div>
 </template>
 
@@ -11,7 +30,20 @@ console.log(runtimeConfig)
 </script>
 
 <script>
+export default {
+    data() {
+        return {
+            url: "",
+            shortenedURL: ""
+        }
+    },
+    methods: {
+        async shortenURL() {
+            console.log("Shortening URL")
+            console.log(this.url)
+        },
+    },
+}
 </script>
 
-<style>
-</style>
+<style></style>
