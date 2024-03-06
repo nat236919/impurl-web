a2209dcf3e025cfa473666d8de88d501066cb611
nuttaphat <nuttaphat.aruno@gmail.com>
2024-03-06 21:29:59 +0800

    feat: call API to get short URL (#1)

--- pages/[id].vue
+++ pages/[id].vue
@@ -1,5 +1,22 @@
 <template>
     <div>
-        <p>ID: {{ $route.params.id }}</p>
+        <p>ID: {{ id }}</p>
+        <p>API URL: {{ apiUrl }}</p>
+        <p>Response: {{ res }}</p>
     </div>
 </template>
+
+<script setup lang="ts">
+import { APIResponse } from "~~/types/APIResponse";
+
+const route = useRoute();
+const runtimeConfig = useRuntimeConfig();
+console.log(route);
+console.log(runtimeConfig);
+
+const id = computed(() => route.params.id);
+const apiUrl = computed(() => `${runtimeConfig.apiHost}/v1/urls/${id.value}/?token=${runtimeConfig.apiSecretKey}`);
+
+const res = await useFetch<APIResponse>(apiUrl.value);
+console.log(res);
+</script>
---
+++ types/APIResponse.ts
@@ -0,0 +1,8 @@
+export type APIResponse = {
+    id: string;
+    original_url: string;
+    user_id: string;
+    expiry_at: number;
+    created_at: number;
+    modified_at: number;
+}
