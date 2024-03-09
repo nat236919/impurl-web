0aa2ccd1eb002c1da0f03d53d3a7579f94296573
nuttaphat <nuttaphat.aruno@gmail.com>
2024-03-09 21:50:08 +0800

    feat: extract data from response (#1)

--- pages/[id].vue
+++ pages/[id].vue
@@ -3,20 +3,35 @@
         <p>ID: {{ id }}</p>
         <p>API URL: {{ apiUrl }}</p>
         <p>Response: {{ res }}</p>
+        <p>Data: {{ data }}</p>
     </div>
 </template>
 
 <script setup lang="ts">
 import { APIResponse } from "~~/types/APIResponse";
 
+// Get Route and Runtime Config
 const route = useRoute();
 const runtimeConfig = useRuntimeConfig();
-console.log(route);
-console.log(runtimeConfig);
 
+// Get ID and API URL
 const id = computed(() => route.params.id);
-const apiUrl = computed(() => `${runtimeConfig.apiHost}/v1/urls/${id.value}/?token=${runtimeConfig.apiSecretKey}`);
+const apiUrl = computed(() => `${runtimeConfig.apiHost}/v1/urls/${id.value}`);
 
-const res = await useFetch<APIResponse>(apiUrl.value);
-console.log(res);
+// Fetch Data
+const res = await useFetch<APIResponse>(apiUrl.value, {
+    method: "GET",
+    headers: {
+        "Content-Type": "application/json",
+        "X-Api-Key": runtimeConfig.apiSecretKey,
+    },
+}).catch((error) => {
+    throw createError({
+        statusCode: 404,
+        message: "URL not found",
+    });
+});
+
+// Get Data from Response
+const data = computed(() => res.data);
 </script>
