a7ccbf65c3fa47accf1191fd68639e94fccb206c
nuttaphat <nuttaphat.aruno@gmail.com>
2024-03-11 22:19:14 +0800

    feat: meta and SEO data (#2)

--- app.vue
+++ app.vue
@@ -1,3 +1,25 @@
+<script setup lang="ts">
+useHead({
+  title: "impURL - Shorten your URLs",
+  meta: [
+    {
+      name: "ImpURL",
+      content: "Where magic makes links shorter and sweeter"
+    }
+  ],
+});
+
+useSeoMeta({
+  title: "impURL - Shorten your URLs",
+  description: "Where magic makes links shorter and sweeter",
+  image: "/img/imp-logo.png",
+  url: "",
+  type: "website",
+  site_name: "impURL",
+  locale: "en_US",
+});
+</script>
+
 <template>
   <div>
     <NuxtLayout>
