60a2a62a426fcb5363e2c5dd307c5a437268d0b0
nuttaphat <nuttaphat.aruno@gmail.com>
2024-03-13 15:48:01 +0800

    styles: add a new style for the index page (#2)

--- pages/index.vue
+++ pages/index.vue
@@ -84,12 +84,70 @@ async function shrinkUrl() {
 </template>
 
 <style lang="scss">
+html,
 body {
-    background-color: #fcfcfc;
+    height: 100%;
+}
+
+body {
+    background: radial-gradient(ellipse at center, #fffeea 0%, #ffffff 35%, #91abad 100%);
+    overflow: hidden;
+}
+
+.ocean {
+    height: 5%;
+    width: 100%;
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    background: #015871;
+}
+
+.wave {
+    background: url(https://cdn.kcak11.com/codepen_assets/wave_animation/wave.svg) repeat-x;
+    position: absolute;
+    top: -198px;
+    width: 6400px;
+    height: 198px;
+    animation: wave 7s cubic-bezier(0.36, 0.45, 0.63, 0.53) infinite;
+    transform: translate3d(0, 0, 0);
+}
+
+.wave:nth-of-type(2) {
+    top: -175px;
+    animation: wave 7s cubic-bezier(0.36, 0.45, 0.63, 0.53) -0.125s infinite,
+        swell 7s ease -1.25s infinite;
+    opacity: 1;
+}
+
+@keyframes wave {
+    0% {
+        margin-left: 0;
+    }
+
+    100% {
+        margin-left: -1600px;
+    }
+}
+
+@keyframes swell {
+
+    0%,
+    100% {
+        transform: translate3d(0, -25px, 0);
+    }
+
+    50% {
+        transform: translate3d(0, 5px, 0);
+    }
+}
+
+.endWave {
+    display: none;
 }
 
 .logo {
-    max-width: 200px;
+    max-width: 250px;
     animation: wiggle 20s;
     animation-iteration-count: infinite;
 }
