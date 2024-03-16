d158f2955c27b3566314dcf7136cd7aeecb429c2
nuttaphat <nuttaphat.aruno@gmail.com>
2024-03-16 15:22:18 +0800

    feat: redirect to original URL after API call (#2)

--- pages/[id].vue
+++ pages/[id].vue
@@ -1,40 +1,51 @@
 <script setup lang="ts">
 import { APIResponse } from "~~/types/APIResponse";
 
+// Route and ID
 const route = useRoute();
 const id = computed(() => route.params.id);
-const originalUrl = ref(null);
 
-const res = await useFetch<APIResponse>("/api/redirect", {
+// Redirect to original URL
+const originalUrl = ref<string | null>(null);
+originalUrl.value = await useFetch<APIResponse>("/api/redirect", {
     method: "POST",
     body: JSON.stringify({ "id": id.value })
+}).then((response) => {
+    if (response.data.value) {
+        let data = response.data.value;
+        if (!data || !data.original_url) {
+            throw new Error("URL not found");
+        } else {
+            let originalUrl = data.original_url;
+            return originalUrl;
+        }
+
+    } else {
+        throw new Error("No URL found");
+    }
 }).catch((error) => {
-    console.error(error);
     throw createError({
         statusCode: 404,
-        message: "URL not found",
+        message: error,
     });
 });
 
-// Get data from Response
-const data = computed(() => res.data.value);
-
-// Redirect to 404 if data not found
-if (!data.value || !data.value.original_url) {
-    throw createError({
-        statusCode: 404,
-        message: "URL not found",
-    });
-} else {
-    // Redirect to original URL
-    originalUrl.value = computed(() => data.value.original_url);
+if (await originalUrl.value) {
+    await navigateTo(originalUrl.value, {
+        external: true,
+        open: {
+            target: "_blank",
+            windowFeatures: {
+                width: 500,
+                height: 500
+            }
+        }
+    })
 }
 </script>
 
 <template>
     <div>
         Loading...
-        {{ data }}
-        {{ originalUrl }}
     </div>
 </template>
