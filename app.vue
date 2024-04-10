75971ac2f25407e8e1b74b3addd75915666a5486
nuttaphat <nuttaphat.aruno@gmail.com>
2024-04-10 21:26:13 +0800

    refactor: use of Vue 3 Composition API (#6)

--- app.vue
+++ app.vue
@@ -1,17 +1,22 @@
 <script setup lang="ts">
+import { ref } from 'vue';
+
+const title = ref("impURL - Shorten your URLs");
+const description = ref("Where magic makes links shorter and sweeter");
+
 useHead({
-  title: "impURL - Shorten your URLs",
+  title: title.value,
   meta: [
     {
       name: "ImpURL",
-      content: "Where magic makes links shorter and sweeter"
+      content: description.value
     }
   ],
 });
 
 useSeoMeta({
-  title: "impURL - Shorten your URLs",
-  description: "Where magic makes links shorter and sweeter",
+  title: title.value,
+  description: description.value,
   image: "/img/imp-logo.png",
   url: "",
   type: "website",
@@ -38,6 +43,6 @@ body {
 body {
   background: radial-gradient(ellipse at center, #fffffa 0%, #ffffff 35%, #c8dedf 100%);
   overflow: hidden;
-  font-family: Helvetica, Arial, sans-serif !important;
+  font-family: Helvetica, Arial, sans-serif;
 }
 </style>
